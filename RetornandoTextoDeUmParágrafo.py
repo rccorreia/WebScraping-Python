2bc349c9dc23cb039cbfc2e2a344eeb3fa213457
Ricardo Correia <rdcc89@gmail.com>
2017-10-15 22:12:40 -0200

    Refatorando arquivo RetornandoTextoDeUmParágrafo

--- RetornandoTextoDeUmParágrafo.py
+++ RetornandoTextoDeUmParágrafo.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+página = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
 
-soup = BeautifulSoup(page.content, 'html.parser')
-html = list(soup.children)[2]
+beautifulSoup = BeautifulSoup(página.content, 'html.parser')
+html = list(beautifulSoup.children)[2]
 body = list(html.children)[3]
 p = list(body.children)[1]
-textoDoParagrafo = p.get_text()
+textoDoParágrafo = p.get_text()
 
-print(textoDoParagrafo)
+print(textoDoParágrafo)
